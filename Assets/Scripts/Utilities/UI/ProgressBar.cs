9e283613736cb61f4884e00544a55a7574c9fb32
danvar249 <danvar249@gmail.com>
2025-11-18 16:34:06 +0200

    Add reusable ProgressBar UI component
    Introduced a ProgressBar prefab and associated script for displaying progress visually in the UI. Updated the Player scene to include the new ProgressBar under a new 'Stats' UI container. Adjusted sprite import settings for progress bar assets to improve appearance and slicing.

---
+++ Assets/Scripts/Utilities/UI/ProgressBar.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utilities.UI
+{
+    [ExecuteInEditMode]
+    public class ProgressBar : MonoBehaviour
+    {
+        public int minimum;
+        public int maximum;
+        public int current;
+        public Image mask;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GetCurrentFill();
+        }
+
+        void GetCurrentFill()
+        {
+            float currentOffset = current - minimum;
+            float maximumOffset = maximum - minimum;
+            var fillAmount = currentOffset / maximumOffset;
+            mask.fillAmount = fillAmount;
+        }
+    }
+}
